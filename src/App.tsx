1511e624b20e29b24309439914180657c778b163
vijayalaxmiawale-hue <vijayalaxmi.awale@cumminscollege.in>
2026-04-05 14:04:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Route, Routes } from "react-router-dom"; // Changed to HashRouter
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import Scenarios from "./pages/Scenarios.tsx";
-import Communication from "./pages/Communication.tsx";
-import SkillProgress from "./pages/SkillProgress.tsx";
-import SettingsPage from "./pages/Settings.tsx";
-import NotFound from "./pages/NotFound.tsx";
+
+import Index from "./pages/Index";
+import Scenarios from "./pages/Scenarios";
+import Communication from "./pages/Communication";
+import SkillProgress from "./pages/SkillProgress";
+import SettingsPage from "./pages/Settings";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -16,12 +17,14 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner /> 
-      <HashRouter> {/* Changed to HashRouter */}
+      <Sonner />
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/scenarios" element={<Scenarios />} />
           <Route path="/communication" element={<Communication />} />
+          {/* This line below fixes the 'simulation/salary' 404 error */}
+          <Route path="/simulation/:id" element={<Communication />} /> 
           <Route path="/progress" element={<SkillProgress />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="*" element={<NotFound />} />
